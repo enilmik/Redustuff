aca9cf1a5e884e59bc5988eeaed20194de35805b
enilmik <kim@enilmik.dk>
2015-06-07 08:38:33 +0200

    quiz1

--- getdata-015/quiz1.R
+++ getdata-015/quiz1.R
@@ -5,5 +5,34 @@ Idaho2006 <- read.csv("~/Cousera/Getting Data/getdata-data-ss06hid.csv")
 
 table(Idaho2006$VAL)
 # or
-filter(Idaho2006,VAL == 24) # returns 53 rows 
+filter(Idaho2006,VAL == 24) # returns 53 rows
 
+Q3
+
+ library(xlsx)
+
+ colIndex <- 7:15
+ rowIndex <- 18:23
+
+dat <- read.xlsx("NGAP2.xlsx",sheetIndex =1,colIndex=colIndex,rowIndex=rowIndex)
+sum(dat$Zip*dat$Ext,na.rm=T)
+36534720
+
+Q4
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
+
+fileurl = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+direct http read of xml , gives error.
+
+library(tools)
+target_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+target_localfile = "restaurants.xml"
+download.file(target_url, target_localfile, method = "curl")
+md5sum(target_localfile)
+getwd()
+
+fileurl = "restaurants.xml"
+doc <- xmlTreeParse(fileurl,useInternal = TRUE)
+rootNode <- xmlRoot(doc)
+table(xpathSApply(doc,"//zipcode",xmlValue))
